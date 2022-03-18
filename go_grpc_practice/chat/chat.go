992a65d50f89a1f1eb7ba5a38403d5d9d1cf7b3d
Seo-A-Nam <nsa901@naver.com>
2022-03-18 20:28:29 +0900

    1

--- go_grpc_practice/chat/chat.go
+++ go_grpc_practice/chat/chat.go
@@ -9,9 +9,8 @@ type Server struct {
 
 }
  
-func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, 
-error) 
-{ // define rpc methods - returns message pointer or error
+func (s *Server) SayHello(ctx context.Context, message *Message) (*Message,
+error) { // define rpc methods - returns message pointer or error
 	log.Printf("Recieved message body from client: %s", message.body)
 	return &Message{Body: "Hello From the Server!"}, nil // if fail, return nil for error
 }
