a95221dc44d8c7044dc824831ed9a944d751ded6
Seo-A-Nam <nsa901@naver.com>
2022-03-18 19:42:42 +0900

    2022-03-18

---
+++ go_grpc_practice/go.mod
@@ -0,0 +1,10 @@
+module github.com/Seo-A-Nam/Etc/go_grpc_practice
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.5.2
+	github.com/tutorialedge/go-grpc-tutorial v0.0.0-20200509091100-f8d1b5b15b01 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f
+	google.golang.org/grpc v1.45.0
+)
--- go_grpc_practice/server.go
+++ go_grpc_practice/server.go
@@ -4,7 +4,9 @@ package main
 import (
 	"log"
 	"net"
+	"github.com/golang/protobuf/proto"
 	"google.golang.org/gprc"
+	"github.com/tutorialedge/go-grpc-tutorial/chat"
 )
 
 func main() {
@@ -13,8 +15,11 @@ func main() {
 		log.Fatalf("Failed to listen on port 9000: %v", err)
 	}
 	
-	grpcServer := grpc.NewServer()
+	s := chat.Server() // define a new server
 
+	grpcServer := grpc.NewServer() // make grpc server
+
+	chat.RegisterChatServiceServer(grpcServer, &s) // register chat service server to grpc Server
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to server gRPC server over port 9000: %v", err)
 	}
