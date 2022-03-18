0fd7046dc1ffc33e6c724d9fac9af8c1e5aadadd
Seo-A-Nam <nsa901@naver.com>
2022-03-18 20:27:08 +0900

    1

--- go_grpc_practice/chat/chat.go
+++ go_grpc_practice/chat/chat.go
@@ -2,18 +2,16 @@ package chat
 
 import (
 	"log" // log every incoming requests
-
 	"golang.org/x/net/context" // get the context for the request
 )
 
 type Server struct {
 
 }
-
  
-func (s *Server) SayHello(ctx context.Context, message *Message) // define rpc methods - returns message pointer or error
-	(*Message, error) {
-
-		log.Printf("Recieved message body from client: %s", message.body)
-		return &Message{Body: "Hello From the Server!"}, nil // if fail, return nil for error
-	}
+func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, 
+error) 
+{ // define rpc methods - returns message pointer or error
+	log.Printf("Recieved message body from client: %s", message.body)
+	return &Message{Body: "Hello From the Server!"}, nil // if fail, return nil for error
+}
--- go_grpc_practice/go.mod
+++ go_grpc_practice/go.mod
@@ -1,13 +1,16 @@
-module github.com/Seo-A-Nam/Etc/go_grpc/practice
+module github.com/Seo-A-Nam/Etc/go_grpc_practice
 
 go 1.17
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
-	golang.org/x/text v0.3.0 // indirect
+	github.com/golang/protobuf v1.5.2
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f
+	google.golang.org/grpc v1.45.0
+)
+
+require (
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	google.golang.org/grpc v1.45.0 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
 )
